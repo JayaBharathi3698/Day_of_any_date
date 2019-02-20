2df83edcd1e5d07fb0a2bd1cbf2e2b301b210d15
JayaBharathi3698 <32978124+JayaBharathi3698@users.noreply.github.com>
2019-02-20 12:02:28 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,81 +1,19 @@
-#include<stdio.h>
-int main() {
-int dd,mm,yy;
-printf("Hint:The year you entered should be between 1700 to 3000.\n");
-printf("Enter the date,month,year in the format [date month year] :\n ");
-scanf("%d%d%d",&dd,&mm,&yy);
-//to get yearcode
-int yearcode,i;
-int l=(yy%10);
-int k=(yy/10)%10;
-int a=(k*10)+l;
-yearcode=(a/4)+dd;
-//to find monthcode
-int monthcode;
-if(mm==1)
-monthcode=1;
-else if(mm==2)
-monthcode=4;
-else if(mm==3)
-monthcode=4;
-else if(mm==4)
-monthcode=0;
-else if(mm==5)
-monthcode=2;
-else if(mm==6)
-monthcode=5;
-else if(mm==7)
-monthcode=0;
-else if(mm==8)
-monthcode=3;
-else if(mm==9)
-monthcode=6;
-else if(mm==10)
-monthcode=1;
-else if(mm==11)
-monthcode=4;
-else 
-monthcode=6;
-//to find century code
-int centurycode;
-if(yy>1699&&yy<=1799)
-centurycode=0;
-else if(yy>1799&&yy<=1899)
-centurycode=2;
-else if(yy>1899&&yy<=1999)
-centurycode=0;
-else if(yy>1999&&yy<=2999)
-centurycode=6;
-int sum=0;
-//to check if it is leap year or not
-if(yy%4==0){
-    if(yy%100==0){
-        if(yy%400==0)
-        sum=(yearcode-monthcode+centurycode-a)-1;
-        else
-        sum=(yearcode+monthcode+centurycode+a);
+#include <stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(i=1; i <= n; ++i)
+    {
+        sum += i;   // sum = sum+i;
     }
-     else
-    sum=(yearcode+monthcode+centurycode+a)-1;
+
+    printf("Sum = %d",sum);
+
+    return 0;
 }
-else
-sum=(yearcode+monthcode+centurycode+a);
-printf("The date %d:%d:%d which you entered is : ",dd,mm,yy);
-//to calculate day;
-int rem=(sum%7);
-if(rem==0)
-printf("SSSSSSaturday ");
-else if(rem==1)
-printf("SSSSSundayyyyy");
-else if(rem==2)
-printf("MMMMMMondayyyyyy");
-else if(rem==3)
-printf("TTTTTuesdayyyyyyyyyy");
-else if(rem==4)
-printf("WWWWWWWednesdayyyy");
-else if(rem==5)
-printf("TTTTTTThursdayyyyy ");
-else if(rem==6)
-printf("FFFFFFFridayyyy");
-}
+
 
