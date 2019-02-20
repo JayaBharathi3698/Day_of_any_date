0929dca90a8293b3bc35c6fd483680a3e26639e7
JayaBharathi3698 <32978124+JayaBharathi3698@users.noreply.github.com>
2019-02-20 13:04:20 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    int n;
-    printf("Enter a positive integer: ");
-    scanf("%d",&n);
-    if(n==2){
-    printf("POSITIVE NUMBER");
-    }
+  prntf("HELLO, WORLD");
+  return 0;
 }
 
 
